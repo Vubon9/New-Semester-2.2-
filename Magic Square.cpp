ec71239afbe35d19897abed20b8a6455d0d54f33
Fahim Faisal Pathan <vubon2005@gmail.com>
2026-05-02 21:33:09 +0600

    Refactor Magic Square program for clarity and structure

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -1,64 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,k,l;
-    cout<<"Enter the Odd Number: ";
-    cin>>n;
+int main() {
 
-    if(n%2==0){
+    int n, k, l;
+
+    cout << "Enter the Odd Number: ";
+    cin >> n;
+
+    // Check odd number
+    if(n % 2 == 0) {
+        cout << "Please enter an odd number!" << endl;
         return 0;
     }
-    else{
-        int square[50][50];   // fixed size (safe)
 
-        // initialize 0
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                square[i][j]=0;
-            }
-        }
+    // Magic square array
+    int square[50][50];
 
-        int i = 0;        // starting position
-        int j = n/2;
-        square[i][j] = 1; // first value
-
-        for(int key=2; key<=n*n; key++){
-
-            // move up
-            if(i>0){
-                k=i-1;
-            }
-            else{
-                k=n-1;
-            }
-
-            // move right
-            if(j<n-1){
-                l=j+1;
-            }
-            else{
-                l=0;
-            }
-
-            if(square[k][l]>=1){
-                i=(i+1)%n;   // move down
-            }
-            else{
-                i=k;
-                j=l;
-            }
-
-            square[i][j]=key;
-        }
-
-        // print
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cout<<square[i][j]<<" ";
-            }
-            cout<<endl;
+    // Initialize all values with 0
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            square[i][j] = 0;
         }
     }
+
+    // Starting position
+    int i = 0;
+    int j = n / 2;
+
+    square[i][j] = 1;
+
+    // Fill the magic square
+    for(int key = 2; key <= n * n; key++) {
+
+        // Move Up
+        if(i > 0) {
+            k = i - 1;
+        }
+        else {
+            k = n - 1;
+        }
+
+        // Move Left
+        if(j > 0) {
+            l = j - 1;
+        }
+        else {
+            l = n - 1;
+        }
+
+        // Check occupied cell
+        if(square[k][l] >= 1) {
+            i = (i + 1) % n;   // Move Down
+        }
+        else {
+            i = k;
+            j = l;
+        }
+
+        square[i][j] = key;
+    }
+
+    // Print Magic Square
+    cout << "\nMagic Square:\n\n";
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << setw(4) << square[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
